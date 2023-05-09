7aefaff4782cc2c453dee1ec5ff714315a927f50
Main <q1470001@gmail.com>
2023-05-09 13:25:38 +0800

    Update general.js

--- Izayoi/general.js
+++ Izayoi/general.js
@@ -1,4 +1,4 @@
-
+var token = "";
 async function get(url, data) {
 	let res = await fetch(url, {
 		body: data,
@@ -29,4 +29,56 @@ async function post_j(url, data) {
 		method: 'POST',
 	});
 	return await res.text();
+}
+
+async function dget(url, data) {
+	let res = await fetch(url, {
+		body: data,
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded',
+			'Authorization': 'Bot ' + token,
+		},
+		method: 'GET',
+	});
+	return await res.text();
+}
+
+async function dpost_j(url, data) {
+	let res = await fetch(url, {
+		body: data,
+		headers: {
+			'content-type': 'application/json;charset=UTF-8',
+			'Authorization': 'Bot ' + token,
+		},
+		method: 'POST',
+	});
+	return await res.text();
+}
+
+async function dpost(url, data) {
+	let res = await fetch(url, {
+		body: data,
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded',
+			'Authorization': 'Bot ' + token,
+		},
+		method: 'POST',
+	});
+	return await res.text();
+}
+
+async function aget(url) {
+	// 使用 AJAX 调用 Discord API
+	$.ajax({
+		url: url,
+		type: "GET",
+		headers: {
+			"Authorization": "Bot " + token
+		},
+		success: function(response) {
+			console.log(response);
+			return response;
+			// 在此处处理返回的服务器信息
+		}
+	});
 }
